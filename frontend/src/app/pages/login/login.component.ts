1e38406d3514403b1a2ccc3805ebe6fafa48084f
Lázaro Marques <lazaro.marquesx@gmail.com>
2024-11-08 14:50:33 -0300

    feat: Formulários de login e cadastro

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, viewChild } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LoginComponent {
+  swiperElement = viewChild<ElementRef>('swiper');
 
+  public showBackSlide = false;
+  public slideOptions = {
+    initialSlide: 0,
+    speed: 150
+  };
+  
+  async onSlideChange(event: any) {
+    const slides: Swiper = event.target;
+    this.showBackSlide = !slides.isBeginning;
+  }
+
+  get slide(): Swiper {
+    return this.swiperElement()?.nativeElement.swiper;
+  }
 }
