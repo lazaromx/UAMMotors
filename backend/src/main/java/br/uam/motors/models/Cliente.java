3c2c9e9e9f5c1b10761565c0590078297a4c34c9
Lázaro Marques <lazaro.marquesx@gmail.com>
2024-11-21 17:16:06 -0300

    feat: Método de Login de clientes

--- backend/src/main/java/br/uam/motors/models/Cliente.java
+++ backend/src/main/java/br/uam/motors/models/Cliente.java
@@ -84,6 +84,8 @@ public class Cliente {
         this.email = email;
     }
 
+    public String getSenha(){return senha;}
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
--- backend/src/main/java/br/uam/motors/repositories/ClienteRepository.java
+++ backend/src/main/java/br/uam/motors/repositories/ClienteRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
     public Cliente findByEmail(String email);
     public Cliente findByCpf(String cpf);
+
+    public Cliente findByEmailAndSenha(String email, String senha);
 }
